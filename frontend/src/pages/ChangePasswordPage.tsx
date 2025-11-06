44d063aa5de9fcf70107b521d127c6bac5a87887
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-11-06 15:07:03 -0500

    Create ChangePasswordPage.tsx

---
+++ frontend/src/pages/ChangePasswordPage.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from '../components/ChangePassword.tsx';
+
+const ChangePasswordPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordPage;
