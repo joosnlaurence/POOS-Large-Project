b7836feb6c52b4fa7a078dfd32de7bc4b7717772
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-11-06 15:04:46 -0500

    Update App.tsx
    Added ChangePassword

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import PasswordReset from "./pages/PasswordResetPage";
 import VerifySuccessPage from "./pages/VerifySuccessPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/reset-password" element={<PasswordReset />} />
         <Route path="/verify/success" element={<VerifySuccessPage />} />
+        <Route path="/change-password" element={<ChangePasswordPage />} />
       </Routes>
     </BrowserRouter>
   );
