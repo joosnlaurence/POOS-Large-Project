9e7efa9e255d80ceb511d538a883effb53bdfefd
joosnlaurence <jasongotanl@gmail.com>
2025-11-10 07:14:13 -0500

    Add url.js for building URL path

---
+++ backend/utils/url.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+
+export const app_name = '4lokofridays.com';
+export function buildPath(route) {
+    if(process.env.NODE_ENV != 'development'){
+        return `https://${app_name}/${route}`;
+    }
+    else{
+        console.log("localhost");
+        return `http://localhost:5000/${route}`;
+    }
+}
