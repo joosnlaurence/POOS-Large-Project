10b4cdea2bf8bf4b320c32b71e2e1610a31cbc1e
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-11-14 11:44:27 -0500

    Update home_screen.dart

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<void> fetchVerificationStatus() async {
     try {
       final response = await dio.post(
-        '/check-verification',
+        'users/check-verification',
         data: {"email": widget.user.email},
       );
 
